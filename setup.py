78dd902cb62686e9c693b88cddb9ad0ab810fa29
Francesco Romani <fromani@gmail.com>
2014-04-01 23:46:06 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name = 'patchlib',
+      version = '1.14',
+      description = 'Package to process and inspect patches and diffs',
+      license = 'MIT',
+      author = 'Anatoly Techtonik; Francesco Romani',
+      maintainer = 'Francesco Romani',
+      maintainer_email = 'fromani@gmail.com',
+      url = 'http://github.com/mojaves/patchlib',
+      py_modules = [ 'patchlib' ],
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities'
+      ])
